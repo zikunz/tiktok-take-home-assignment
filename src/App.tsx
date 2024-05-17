3a2296491a71902f90f0b16e83915ffb95487910
Zikun Zhu <zhu.zikun@u.nus.edu>
2024-05-18 02:10:02 +0800

    Add TikTok logo on the top left corner

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { Layout, Menu } from 'antd';
 import { AppstoreOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import ProductsPage from './pages/ProductsPage';
 import ShoppingCartPage from './pages/ShoppingCartPage';
+import TikTokLogo from './assets/TikTok.png';
 
 const { Header, Content } = Layout;
 
 const App: React.FC = () => (
   <Router>
     <Layout>
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+      <Header style={{ display: 'flex', alignItems: 'center', padding: '0 0' }}>
+        <a href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={TikTokLogo} alt="TikTok Logo" style={{ width: '80px', height: 'auto' }} />
+        </a>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ flex: 1 }}>
           <Menu.Item key="1" icon={<AppstoreOutlined />}>
             <Link to="/products">Products</Link>
           </Menu.Item>
@@ -20,7 +24,7 @@ const App: React.FC = () => (
           </Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '0 50px'}}>
+      <Content style={{ padding: '0 80px', backgroundColor: '#f0f2f5' }}>
         <div className="site-layout-content">
           <Routes>
             <Route path="/products" element={<ProductsPage />} />
