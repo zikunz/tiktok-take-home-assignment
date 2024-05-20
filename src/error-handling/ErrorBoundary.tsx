212e6cb1b4a059c63c1e2bd0cbdd1169f7bbccf1
Zikun Zhu <zhu.zikun@u.nus.edu>
2024-05-20 16:36:53 +0800

    Add error handling logic

---
+++ src/error-handling/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { notification } from 'antd';
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+    notification.error({
+      message: 'Error',
+      description: 'Something went wrong.',
+    });
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
